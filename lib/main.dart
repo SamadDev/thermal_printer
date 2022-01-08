5d1bb2f7759f4caa0d411921fb59a24282d56aee
Dev <samad.00293265@gmail.com>
2022-01-08 22:01:21 +0300

    firt commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(const ThermalPrinter());
+
+class ThermalPrinter extends StatelessWidget {
+  const ThermalPrinter({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
