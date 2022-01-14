79138872bad86caf72c290b8370ef1b4eb681c2e
Dev <samad.00293265@gmail.com>
2022-01-15 01:42:55 +0300

    screenshot of widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
-
 import 'dart:async';
 import 'dart:convert';
+import 'dart:typed_data';
 import 'package:bluetooth_print/bluetooth_print.dart';
 import 'package:bluetooth_print/bluetooth_print_model.dart';
 import 'package:flutter/material.dart';
+import 'package:screenshot/screenshot.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   _MyAppState createState() => _MyAppState();
 }
@@ -28,9 +31,9 @@ class _MyAppState extends State<MyApp> {
 
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initBluetooth() async {
-    bluetoothPrint.startScan(timeout: Duration(seconds: 4));
+    bluetoothPrint.startScan(timeout: const Duration(seconds: 4));
 
-    bool? isConnected=await bluetoothPrint.isConnected;
+    bool? isConnected = await bluetoothPrint.isConnected;
 
     bluetoothPrint.state.listen((state) {
       print('cur device status: $state');
@@ -55,153 +58,251 @@ class _MyAppState extends State<MyApp> {
 
     if (!mounted) return;
 
-    if(isConnected!) {
+    if (isConnected!) {
       setState(() {
-        _connected=true;
+        _connected = true;
       });
     }
   }
 
-  // List<int> encodeUtf(string) {
-  //   return utf8.encode(string);
-  // }
-  // decodeUtf(string){
-  //   return utf8.decode(string);
-  // }
+  Uint8List? bytes;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('BluetoothPrint example app'),
-        ),
-        body: RefreshIndicator(
-          onRefresh: () =>
-              bluetoothPrint.startScan(timeout: Duration(seconds: 4)),
-          child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                      child: Text(tips),
-                    ),
-                  ],
-                ),
-                StreamBuilder<List<BluetoothDevice>>(
-                  stream: bluetoothPrint.scanResults,
-                  initialData: [],
-                  builder: (c, snapshot) => Column(
-                    children: snapshot.data!.map((d) => ListTile(
-                      title: Text(d.name??''),
-                      subtitle: Text(d.address!),
-                      onTap: () async {
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: MaterialApp(
+        builder: (ctx, child) {
+          return Directionality(
+            textDirection: TextDirection.rtl,
+            child: child!,
+          );
+        },
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('BluetoothPrint example app'),
+          ),
+          body: RefreshIndicator(
+            onRefresh: () =>
+                bluetoothPrint.startScan(timeout: const Duration(seconds: 4)),
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  if (bytes != null)
+                    Container(color: Colors.white, child: Image.memory(bytes!)),
+                  TextButton(
+                      onPressed: () async {
+                        final byte = await ScreenshotController()
+                            .captureFromWidget(Container(color: Colors.white,
+                          height:200,
+
+                          margin: const EdgeInsets.all(5),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: const [
+                                  Text(
+                                    'ناوی بەکارهێنەر:',
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    'کاکە مەند یاسین',
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                              const Divider(color: Colors.black,),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: const [
+                                  Text(
+                                    '٤',
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    ': ژمارەی ئەمپێر',
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                              const Divider(color: Colors.black,),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: const [
+                                  Text(
+                                    '٢٥،٠٠٠',
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    ': تێچوو',
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                              const Divider(color: Colors.black,),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: const [
+                                  Text(
+                                    '١١/١٢/٢٠٢٢',
+                                    style: TextStyle(
+                                        fontSize: 12,
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    ' :مانگی',
+                                    style: TextStyle(
+                                        fontSize: 12,
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ));
                         setState(() {
-                          _device = d;
+                          bytes = byte;
+                          print(byte);
+                          print('____________________');
+                          print(bytes);
                         });
                       },
-                      trailing: _device!=null && _device!.address == d.address?Icon(
-                        Icons.check,
-                        color: Colors.green,
-                      ):null,
-                    )).toList(),
-                  ),
-                ),
-                Divider(),
-                Container(
-                  padding: EdgeInsets.fromLTRB(20, 5, 20, 10),
-                  child: Column(
+                      child: const Icon(Icons.eighteen_mp)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          OutlinedButton(
-                            child: Text('connect'),
-                            onPressed:  _connected?null:() async {
-                              if(_device!=null && _device!.address !=null){
-                                await bluetoothPrint.connect(_device!);
-                              }else{
-                                setState(() {
-                                  tips = 'please select device';
-                                });
-                                print('please select device');
-                              }
-                            },
-                          ),
-                          SizedBox(width: 10.0),
-                          OutlinedButton(
-                            child: Text('disconnect'),
-                            onPressed:  _connected?() async {
-                              await bluetoothPrint.disconnect();
-                            }:null,
-                          ),
-                        ],
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 10, horizontal: 10),
+                        child: Text(tips),
                       ),
-                      OutlinedButton(
-                        child:const Text('print receipt(esc)'),
-                        onPressed:  _connected?() async {
-                          Map<String, dynamic> configL = {};
-                          List<LineText> list = [];
-
-                          configL['charset'] = 'utf-8';
-                          list.add(LineText(type: LineText.TYPE_TEXT, content:  'الموظع',weight: 1, align: LineText.ALIGN_CENTER,linefeed: 1));
-                          list.add(LineText(type: LineText.TYPE_TEXT, content: 'محمود' , weight: 0, align: LineText.ALIGN_LEFT,linefeed: 1));
-                          list.add(LineText(linefeed: 1));
-                          print(list);
-                          await bluetoothPrint.printReceipt(configL, list);
-
-                        }:null,
-                      ),
-                      // OutlinedButton(
-                      //   child: Text('print label(tsc)'),
-                      //   onPressed:  _connected?() async {
-                      //     Map<String, dynamic> config = Map();
-                      //     config['width'] = 40; // 标签宽度，单位mm
-                      //     config['height'] = 70; // 标签高度，单位mm
-                      //     config['gap'] = 2; // 标签间隔，单位mm
-                      //     config['charset'] = 'UTF-8';
-                      //     List<LineText> list = [];
-                      //     list.add(LineText(type: LineText.TYPE_TEXT, x:10, y:10, content: 'الموظع'));
-                      //     list.add(LineText(type: LineText.TYPE_TEXT, x:10, y:40, content: 'تیست'));
-                      //     list.add(LineText(type: LineText.TYPE_QRCODE, x:10, y:70, content: 'تیست\n'));
-                      //     list.add(LineText(type: LineText.TYPE_BARCODE, x:10, y:190, content: 'تیست\n'));
-                      //
-                      //     List<LineText> list1 = [];
-                      //     ByteData data = await rootBundle.load("assets/images/guide3.png");
-                      //     List<int> imageBytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
-                      //     String base64Image = base64Encode(imageBytes);
-                      //     list1.add(LineText(type: LineText.TYPE_IMAGE, x:10, y:10, content: base64Image,));
-                      //
-                      //     await bluetoothPrint.printLabel(config, list);
-                      //     await bluetoothPrint.printLabel(config, list1);
-                      //   }:null,
-                      // ),
                     ],
                   ),
-                )
-              ],
+                  StreamBuilder<List<BluetoothDevice>>(
+                    stream: bluetoothPrint.scanResults,
+                    initialData: const [],
+                    builder: (c, snapshot) => Column(
+                      children: snapshot.data!
+                          .map((d) => ListTile(
+                                title: Text(d.name ?? ''),
+                                subtitle: Text(d.address!),
+                                onTap: () async {
+                                  setState(() {
+                                    _device = d;
+                                  });
+                                },
+                                trailing: _device != null &&
+                                        _device!.address == d.address
+                                    ? const Icon(
+                                        Icons.check,
+                                        color: Colors.green,
+                                      )
+                                    : null,
+                              ))
+                          .toList(),
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(20, 5, 20, 10),
+                    child: Column(
+                      children: <Widget>[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            OutlinedButton(
+                              child: const Text('connect'),
+                              onPressed: _connected
+                                  ? null
+                                  : () async {
+                                      if (_device != null &&
+                                          _device!.address != null) {
+                                        await bluetoothPrint.connect(_device!);
+                                      } else {
+                                        setState(() {
+                                          tips = 'please select device';
+                                        });
+                                        print('please select device');
+                                      }
+                                    },
+                            ),
+                            OutlinedButton(
+                              child: const Text('disconnect'),
+                              onPressed: _connected
+                                  ? () async {
+                                      await bluetoothPrint.disconnect();
+                                    }
+                                  : null,
+                            ),
+                          ],
+                        ),
+                        OutlinedButton(
+                          child: const Text('print label(tsc)'),
+                          onPressed: _connected
+                              ? () async {
+                                  Map<String, dynamic> config = {};
+                                  List<LineText> list1 = [];
+
+                                  List<int> imageBytes = bytes!.buffer
+                                      .asUint8List(bytes!.offsetInBytes,
+                                          bytes!.lengthInBytes);
+                                  String base64Image = base64Encode(imageBytes);
+                                  list1.add(LineText(align: LineText.ALIGN_CENTER, linefeed: 1,
+                                    type: LineText.TYPE_IMAGE,
+                                    x: 30,
+                                    y: 1,
+                                    content: base64Image,
+                                  ));
+                                  await bluetoothPrint.printLabel(config, list1);
+                                }
+                              : null,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
-        ),
-        floatingActionButton: StreamBuilder<bool>(
-          stream: bluetoothPrint.isScanning,
-          initialData: false,
-          builder: (c, snapshot) {
-            if (snapshot.data!) {
-              return FloatingActionButton(
-                child: Icon(Icons.stop),
-                onPressed: () => bluetoothPrint.stopScan(),
-                backgroundColor: Colors.red,
-              );
-            } else {
-              return FloatingActionButton(
-                  child: Icon(Icons.search),
-                  onPressed: () => bluetoothPrint.startScan(timeout: Duration(seconds: 4)));
-            }
-          },
+          floatingActionButton: StreamBuilder<bool>(
+            stream: bluetoothPrint.isScanning,
+            initialData: false,
+            builder: (c, snapshot) {
+              if (snapshot.data!) {
+                return FloatingActionButton(
+                  child: const Icon(Icons.stop),
+                  onPressed: () => bluetoothPrint.stopScan(),
+                  backgroundColor: Colors.red,
+                );
+              } else {
+                return FloatingActionButton(
+                    child: const Icon(Icons.search),
+                    onPressed: () => bluetoothPrint.startScan(
+                        timeout: const Duration(seconds: 4)));
+              }
+            },
+          ),
         ),
       ),
     );
